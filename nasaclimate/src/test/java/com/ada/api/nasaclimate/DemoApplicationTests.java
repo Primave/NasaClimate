76f445e6d5791cf8fb89e0aaa9a28c522acfedd8
primave <iyamoura@hotmail.com>
2020-07-03 10:12:33 -0300

    Creacion de springboot e maven base para construccion de la restapi

---
+++ nasaclimate/src/test/java/com/ada/api/nasaclimate/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.api.nasaclimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
